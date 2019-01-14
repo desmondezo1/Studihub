94d1d68eeb3a132d27df6da1352102c9e4c3f1d2
Precious Ogbewi <p.ogbewi@gmail.com>
2019-01-14 21:12:52 +0100

    updated migration --add foreign key constraints

--- app/Http/Controllers/Tutor/Auth/RegisterController.php
+++ app/Http/Controllers/Tutor/Auth/RegisterController.php
@@ -35,7 +35,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/tutor';
+    protected $redirectTo = '/successfull';
 
     public function __construct()
     {
--- app/Http/Controllers/Tutor/Auth/VerificationController.php
+++ app/Http/Controllers/Tutor/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/student';
 
     /**
      * Create a new controller instance.
