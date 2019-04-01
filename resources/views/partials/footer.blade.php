5e8d97370f93fe06a226b8fedee3037bf89146b1
Desmondezo1 <desezo@gmail.com>
2019-04-01 20:31:51 +0100

    footer styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,38 +5,53 @@
     <div class="container-fluid text-center text-md-left">
 
         <!-- Grid row -->
-        <div class="row">
+        <div class="row" style="
+        margin-top: 50px;
+        padding: 30px 10px 10px 10px;
+        background: linear-gradient(1deg, #061c3e, #dc3545);
+        box-shadow: 0px -5px 15px #ccc;
+        ">
 
             <!-- Grid column -->
-            <div class="col-md-6 mt-md-0 mt-3">
+            <div class="col-md-6 mt-md-0 mt-3" style="color: #fff;">
 
                 <!-- Content -->
-                <h5 class="text-uppercase">Footer Content</h5>
-                <p>Here you can use rows and columns here to organize your footer content.</p>
-
+               
+                <img class="img-responsive" style="max-height:150px" src="img/logo.png" alt="studihub logo">
+                <h5 class="text-uppercase">STUDIHUB</h5>
+                <p>Helping students learn better!</p>
             </div>
             <!-- Grid column -->
 
             <hr class="clearfix w-100 d-md-none pb-3">
 
             <!-- Grid column -->
-            <div class="col-md-3 mb-md-0 mb-3">
+            <div class="col-md-3 mb-md-0 mb-3" style="color: #e8d7d7;">
 
                 <!-- Links -->
-                <h5 class="text-uppercase">Links</h5>
+                <h5 class="text-uppercase" style="text-decoration: underline;">Quick Links</h5>
 
-                <ul class="list-unstyled">
+                <ul class="list-unstyled footer-list">
                     <li>
-                        <a href="#!">Link 1</a>
+                        <a href="#!">Login</a>
                     </li>
                     <li>
-                        <a href="#!">Link 2</a>
+                        <a href="#!">Register</a>
                     </li>
                     <li>
-                        <a href="#!">Link 3</a>
+                        <a href="#!">Blog</a>
                     </li>
                     <li>
-                        <a href="#!">Link 4</a>
+                        <a href="#!">Practice</a>
+                    </li>
+                    <li>
+                        <a href="#!">Studihub Careers</a>
+                    </li>
+                    <li>
+                        <a href="#!">Become a Tutor</a>
+                    </li>
+                    <li>
+                        <a href="#!">Hire a Private Tutor</a>
                     </li>
                 </ul>
 
@@ -44,23 +59,23 @@
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-3 mb-md-0 mb-3">
+            <div class="col-md-3 mb-md-0 mb-3" style="color: #e8d7d7;">
 
                 <!-- Links -->
-                <h5 class="text-uppercase">Links</h5>
+                <h5 class="text-uppercase" style="text-decoration: underline;">Socials</h5>
 
-                <ul class="list-unstyled">
+                <ul class="list-unstyled footer-list">
                     <li>
-                        <a href="#!">Link 1</a>
+                        <a href="#!">Instagram</a>
                     </li>
                     <li>
-                        <a href="#!">Link 2</a>
+                        <a href="#!">Facebook</a>
                     </li>
                     <li>
-                        <a href="#!">Link 3</a>
+                        <a href="#!">Twitter</a>
                     </li>
                     <li>
-                        <a href="#!">Link 4</a>
+                        <a href="#!">Youtube</a>
                     </li>
                 </ul>
 
@@ -74,7 +89,8 @@
     <!-- Footer Links -->
 
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3"><a href="{{ route('home') }}"> STUDIHUB EDUCATION SERVICES</a> &copy; {{ "2018" != date("Y")  ? "2018 - " . date("Y") : "2018" }}
+    <div class="footer-copyright text-center py-3" style="background: #061c3e;color: #ccc;">
+        <a href="{{ route('home') }}" style="color: #e9ecef;"> STUDIHUB EDUCATION SERVICES</a> &copy; {{ "2018" != date("Y")  ? "2018 - " . date("Y") : "2018" }}
     </div>
     <!-- Copyright -->
 
